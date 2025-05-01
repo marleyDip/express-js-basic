cef8f4c0e4b264482695aeef3eedbd40404931fe
Md Sofian Hasan <Dip.akand9899@gmail.com>
2025-05-01 14:28:05 +0600

    Templating Engine

--- index.js
+++ index.js
@@ -3,29 +3,13 @@ const app = express();
 
 const PORT = 3000;
 
-app.use((req, res, next) => {
-  console.log("start");
-
-  res.on("finish", () => {
-    console.log("End");
-  });
-  next();
-});
+// set ejs as the view engine
+app.set("view engine", "ejs"); // use EJS as the templating engine
 
 app.get("/", (req, res) => {
-  console.log("middleware");
-  res.send("hello, express!");
+  const userName = "Dip Akand";
+  res.render("index", { userName });
 });
-
-app.get("/error", () => {
-  throw new Error("this is test error");
-});
-
-app.use((err, req, res, next) => {
-  console.error(err.message);
-  res.send("internal  server error");
-});
-
 app.listen(PORT, () => {
   console.log(`server is running on http://localhost:${PORT}`);
 });
--- index1.js
+++ index1.js
@@ -37,6 +37,30 @@ app.get("/search", searchController);
 // both
 app.get("/user/:id", filterId);
 
+// Middleware
+app.use((req, res, next) => {
+  console.log("start");
+
+  res.on("finish", () => {
+    console.log("End");
+  });
+  next();
+});
+
+app.get("/", (req, res) => {
+  console.log("middleware");
+  res.send("hello, express!");
+});
+
+app.get("/error", () => {
+  throw new Error("this is test error");
+});
+
+app.use((err, req, res, next) => {
+  console.error(err.message);
+  res.send("internal  server error");
+});
+
 app.listen(PORT, () => {
   console.log(`server is running on http://localhost:${PORT}`);
 });
