31380f68479cbb21b1b61410d45194b5f60057e9
Md Sofian Hasan <Dip.akand9899@gmail.com>
2025-05-01 13:48:09 +0600

    Error Handler Middleware

--- index.js
+++ index.js
@@ -3,17 +3,27 @@ const app = express();
 
 const PORT = 3000;
 
-app.use("/welcome", (req, res, next) => {
-  console.log("A new request received at" + "-" + Date.now());
+app.use((req, res, next) => {
+  console.log("start");
+
+  res.on("finish", () => {
+    console.log("End");
+  });
   next();
 });
 
 app.get("/", (req, res) => {
+  console.log("middleware");
   res.send("hello, express!");
 });
 
-app.get("/welcome", (req, res) => {
-  res.send("welocime to express app");
+app.get("/error", () => {
+  throw new Error("this is test error");
+});
+
+app.use((err, req, res, next) => {
+  console.error(err.message);
+  res.send("internal  server error");
 });
 
 app.listen(PORT, () => {
