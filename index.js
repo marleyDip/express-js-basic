6de4750ff67cad74fad981534249e25b902effb0
Md Sofian Hasan <Dip.akand9899@gmail.com>
2025-05-01 13:19:23 +0600

    Application Level Middleware

--- index.js
+++ index.js
@@ -3,24 +3,17 @@ const app = express();
 
 const PORT = 3000;
 
-app.get("/", (req, res) => {
-  res.send("hello, express?React Full Stack Developer");
+app.use("/welcome", (req, res, next) => {
+  console.log("A new request received at" + "-" + Date.now());
+  next();
 });
 
-app.use(express.json());
+app.get("/", (req, res) => {
+  res.send("hello, express!");
+});
 
-app.get("/things/:name/:id", (req, res) => {
-  const { name, id } = req.params;
-
-  if (!/^\d{5}$/.test(id)) {
-    return res.status(400).json({
-      error:
-        "ID must be exactly 5 digits (0-9). No letters, symbols, or spaces allowed.",
-      receivedId: id,
-    });
-  }
-
-  res.json({ name, id });
+app.get("/welcome", (req, res) => {
+  res.send("welocime to express app");
 });
 
 app.listen(PORT, () => {
