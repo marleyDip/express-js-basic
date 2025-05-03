e1db66e61005dcf30c54f25d5d59192c17b8f487
Md Sofian Hasan <Dip.akand9899@gmail.com>
2025-05-03 20:21:34 +0600

    Session Management

--- index1.js
+++ index1.js
@@ -188,6 +188,29 @@ app.post("/person", async (req, res) => {
   res.send("Person Added");
 });
 
+// Cookie Parser & Session
+import cookieParser from "cookie-parser";
+app.use(cookieParser());
+
+app.get("/", (req, res) => {
+  /* maxAge: 900000 means the cookie will persist for 15 minutes in the browser.
+
+  It's stored and sent with requests until it expires.*/
+
+  res.cookie("name", "express-app");
+  res.send("hello, express");
+});
+
+app.get("/fetch", (req, res) => {
+  console.log(req.cookies);
+  res.send("Api called");
+});
+
+app.get("/remove-cookie", (req, res) => {
+  res.clearCookie("name");
+  res.send("cookie removed");
+});
+
 app.listen(PORT, () => {
   console.log(`server is running on http://localhost:${PORT}`);
 });
