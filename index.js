2959e6c5de76decfb84786d6d7fdbd9a603d34bf
Md Sofian Hasan <Dip.akand9899@gmail.com>
2025-05-03 20:21:07 +0600

    Session Management, User Tracking

--- index.js
+++ index.js
@@ -1,27 +1,36 @@
 import express from "express";
 import cookieParser from "cookie-parser";
+import session from "express-session";
 
 export const app = express();
 const PORT = 3000;
 app.use(cookieParser());
+app.use(
+  session({
+    secret: "sample_secret",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
 
 app.get("/", (req, res) => {
-  /* maxAge: 900000 means the cookie will persist for 15 minutes in the browser.
-
-  It's stored and sent with requests until it expires.*/
-
-  res.cookie("name", "express-app");
   res.send("hello, express");
 });
 
-app.get("/fetch", (req, res) => {
-  console.log(req.cookies);
-  res.send("Api called");
+app.get("/visit", (req, res) => {
+  if (req.session.visitCount) {
+    req.session.visitCount++;
+
+    res.send(`You have visited this page ${req.session.visitCount} times`);
+  } else {
+    req.session.visitCount = 1;
+    res.send("This is your first visit to this page");
+  }
 });
 
-app.get("/remove-cookie", (req, res) => {
-  res.clearCookie("name");
-  res.send("cookie removed");
+app.get("/remove-visit", (req, res) => {
+  req.session.destroy();
+  res.send("Session destroyed. Visit count removed.");
 });
 
 app.listen(PORT, () => {
