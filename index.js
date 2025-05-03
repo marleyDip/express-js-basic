a8b0bb2b80fe743b0e541f00e3d844fd07c00b5e
Md Sofian Hasan <Dip.akand9899@gmail.com>
2025-05-03 10:02:02 +0600

    Cookie Parser

--- index.js
+++ index.js
@@ -6,8 +6,22 @@ const PORT = 3000;
 app.use(cookieParser());
 
 app.get("/", (req, res) => {
+  /* maxAge: 900000 means the cookie will persist for 15 minutes in the browser.
+
+  It's stored and sent with requests until it expires.*/
+
   res.cookie("name", "express-app");
   res.send("hello, express");
+});
+
+app.get("/fetch", (req, res) => {
+  console.log(req.cookies);
+  res.send("Api called");
+});
+
+app.get("/remove-cookie", (req, res) => {
+  res.clearCookie("name");
+  res.send("cookie removed");
 });
 
 app.listen(PORT, () => {
